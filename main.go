ebc324b3d0c189bc1e319daad3205f2800d144d1
Damien Metzler <dmetzler@nuxeo.com>
2014-06-10 16:27:05 +0200

    NXIO-209 handle SIGUSR1 by dumping goroutine stacks

--- main.go
+++ main.go
@@ -5,24 +5,38 @@ import (
 	"os/signal"
 	"syscall"
 	"github.com/golang/glog"
+	"runtime/pprof"
 )
 
 func main() {
 	c := parseConfig()
 
-	//Exit gracefully
-	exit := make(chan os.Signal, 1)
-	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-exit
-		glog.Info("Shutting down...")
-		os.Exit(0)
-	}()
+	handleSignals()
 
 	b := NewBackends(c)
+
 	b.Init()
 	glog.Info("Starting proxy")
 	p := NewTCPProxy(c, b)
 	p.start()
+
+}
+
+
+func handleSignals() {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(signals, os.Interrupt, syscall.SIGUSR1)
+	go func() {
+		sig := <-signals
+		switch sig {
+		case syscall.SIGTERM, syscall.SIGINT:
+			//Exit gracefully
+			glog.Info("Shutting down...")
+			os.Exit(0)
+		case syscall.SIGUSR1:
+			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+		}
+	}()
 
 }
